6b0ec581ea4500fa212640f6b40ce79cd4041209
Dr4gonJnuit <jonas.bette@student.uclouvain.be>
2024-03-06 23:24:07 +0100

    Fin des fct mais passse pas test ingi

--- template_alphabeta.py
+++ template_alphabeta.py
@@ -62,8 +62,21 @@ class AlphaBetaAgent(Agent):
         Returns:
             float: The evaluated score of the state.
         """
+        agent_id = self.player 
+        # TODO: Delete this comment... It's just so you can laugh at me a bit :-p
+        # The AplhaBetaAgent class is a subclass of the Agent class, so it has the player attribute
+        # I've taked to many time to understand that we can access to the player attribute from the Agent class, lol
+        
+        player_to_play = state.to_move # Can be 0 or 1 (Not very useful but it's just for better understanding) -> Can be placed in the if statement below
+        
         pieces_player, pieces_opponent = self.nmb_of_pieces(state)
-        return pieces_player - pieces_opponent
+        
+        # If the next player is the agent, that mean the opponent is the agent. Since we want to calculate the score
+        # from the perspective of the agent, we need to substract the number of pieces of the opponent that is the agent
+        if player_to_play == agent_id:
+            return pieces_opponent - pieces_player
+        else:
+            return pieces_player - pieces_opponent
     
     def nmb_of_pieces(self, state): # Not in original file
         """Returns the number of pieces on the board for the agent's player and the opponent's player.
@@ -102,6 +115,9 @@ class AlphaBetaAgent(Agent):
         This method recursively explores all possible actions from the current state to find the one that maximizes
         the player's score, pruning branches that cannot possibly affect the final decision.
 
+        See: page 196 of the book "Artificial Intelligence: A Modern Approach" by Stuart Russell and Peter Norvig
+             for the pseudo-code of the MAX-VALUE.
+        
         Args:
             state (ShobuState): The current state of the game.
             alpha (float): The current alpha value, representing the minimum score that the maximizing player is assured of.
@@ -112,7 +128,16 @@ class AlphaBetaAgent(Agent):
             tuple: A tuple containing the best value achievable from this state and the action that leads to this value.
                 If the state is a terminal state or the depth limit is reached, the action will be None.
         """
-        ...
+        if self.is_cutoff(state, depth):
+            return (self.eval(state), None)
+        
+        v, move = alpha, None
+        for action in self.actions():
+            v2, a2 = self.min_value(state, alpha, self.eval(state), depth + 1)
+            if v2 > v:
+                v, move = v2, action
+        
+        return (v, move)
 
 
     def min_value(self, state, alpha, beta, depth):
@@ -121,6 +146,9 @@ class AlphaBetaAgent(Agent):
         Similar to max_value, this method recursively explores all possible actions from the current state to find
         the one that minimizes the opponent's score, again using alpha-beta pruning to cut off branches that won't
         affect the outcome.
+        
+        See: page 196 of the book "Artificial Intelligence: A Modern Approach" by Stuart Russell and Peter Norvig
+             for the pseudo-code of the MIN-VALUE.
 
         Args:
             state (ShobuState): The current state of the game.
@@ -132,4 +160,13 @@ class AlphaBetaAgent(Agent):
             tuple: A tuple containing the best value achievable from this state for the opponent and the action that leads to this value.
                 If the state is a terminal state or the depth limit is reached, the action will be None.
         """
-        ...
+        if self.is_cutoff(state, depth):
+            return (self.eval(state), None)
+        
+        v, move = beta, None
+        for action in self.actions():
+            v2, a2 = self.max_value(state, self.eval(state), beta, depth + 1)
+            if v2 < v:
+                v, move = v2, action
+        
+        return (v, move)

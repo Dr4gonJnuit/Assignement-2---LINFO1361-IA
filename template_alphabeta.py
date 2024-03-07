e0c8c7f402a46b968fe8a15f98199873c394e49f
Dr4gonJnuit <jonas.bette@student.uclouvain.be>
2024-03-07 22:11:11 +0100

    Correct Algo mais infinite boucle

--- template_alphabeta.py
+++ template_alphabeta.py
@@ -45,7 +45,7 @@ class AlphaBetaAgent(Agent):
         Returns:
             bool: True if the search should be cut off, False otherwise.
         """
-        if state.is_terminal(): # That's mean the game is over
+        if self.game.is_terminal(state): # That's mean the game is over
             return True
         
         if self.max_depth <= depth: # That's mean the search tree is too deep
@@ -62,40 +62,22 @@ class AlphaBetaAgent(Agent):
         Returns:
             float: The evaluated score of the state.
         """
-        agent_id = self.player 
-        # TODO: Delete this comment... It's just so you can laugh at me a bit :-p
-        # The AplhaBetaAgent class is a subclass of the Agent class, so it has the player attribute
-        # I've taked to many time to understand that we can access to the player attribute from the Agent class, lol
-        
+        agent_id = self.player
         player_to_play = state.to_move # Can be 0 or 1 (Not very useful but it's just for better understanding) -> Can be placed in the if statement below
         
-        pieces_player, pieces_opponent = self.nmb_of_pieces(state)
+        pieces_player, pieces_opponent = 0.0, 0.0
+        
+        board = state.board
+        for _, home_board in enumerate(board): # _ is the home_board id
+            pieces_player += len(home_board[0])
+            pieces_opponent += len(home_board[1])
         
         # If the next player is the agent, that mean the opponent is the agent. Since we want to calculate the score
         # from the perspective of the agent, we need to substract the number of pieces of the opponent that is the agent
         if player_to_play == agent_id:
-            return float(pieces_opponent - pieces_player)
+            return pieces_opponent - pieces_player
         else:
-            return float(pieces_player - pieces_opponent)
-    
-    def nmb_of_pieces(self, state): # Not in original file
-        """Returns the number of pieces on the board for the agent's player and the opponent's player.
-
-        Args:
-            state (ShobuState): The game state to evaluate.
-
-        Returns:
-            tuple: A tuple of integers representing the number of pieces for the agent's player and the opponent's player.
-        """
-        pieces_player, pieces_opponent = 0, 0
-        
-        board = state.board
-        for home_board in enumerate(board):
-            pieces_player += len(home_board[0])
-            pieces_opponent += len(home_board[1])
-        
-        return (pieces_player, pieces_opponent)
-        ...
+            return pieces_player - pieces_opponent
 
     def alpha_beta_search(self, state):
         """Implements the alpha-beta pruning algorithm to find the best action.
@@ -115,7 +97,7 @@ class AlphaBetaAgent(Agent):
         This method recursively explores all possible actions from the current state to find the one that maximizes
         the player's score, pruning branches that cannot possibly affect the final decision.
 
-        See: page 196 of the book "Artificial Intelligence: A Modern Approach" by Stuart Russell and Peter Norvig
+        See: page 200 of the book "Artificial Intelligence: A Modern Approach" by Stuart Russell and Peter Norvig
              for the pseudo-code of the MAX-VALUE.
         
         Args:
@@ -129,13 +111,16 @@ class AlphaBetaAgent(Agent):
                 If the state is a terminal state or the depth limit is reached, the action will be None.
         """
         if self.is_cutoff(state, depth):
-            return (self.eval(state), None)
+            return (self.game.utility(state, self.player), None)
         
-        v, move = alpha, None
-        for action in self.actions():
-            v2, a2 = self.min_value(state, alpha, self.eval(state), depth + 1)
+        v, move = -float("inf"), None
+        for action in self.game.actions(state):
+            v2, a2 = self.min_value(self.game.result(state, action), alpha, beta, depth + 1)
             if v2 > v:
                 v, move = v2, action
+                aplha = max(alpha, v)
+            if v >= beta:
+                return (v, move)
         
         return (v, move)
 
@@ -147,7 +132,7 @@ class AlphaBetaAgent(Agent):
         the one that minimizes the opponent's score, again using alpha-beta pruning to cut off branches that won't
         affect the outcome.
         
-        See: page 196 of the book "Artificial Intelligence: A Modern Approach" by Stuart Russell and Peter Norvig
+        See: page 200 of the book "Artificial Intelligence: A Modern Approach" by Stuart Russell and Peter Norvig
              for the pseudo-code of the MIN-VALUE.
 
         Args:
@@ -161,12 +146,15 @@ class AlphaBetaAgent(Agent):
                 If the state is a terminal state or the depth limit is reached, the action will be None.
         """
         if self.is_cutoff(state, depth):
-            return (self.eval(state), None)
+            return (self.game.utility(state, self.player), None)
         
-        v, move = beta, None
-        for action in self.actions():
-            v2, a2 = self.max_value(state, self.eval(state), beta, depth + 1)
+        v, move = float("inf"), None
+        for action in self.game.actions(state):
+            v2, a2 = self.max_value(self.game.result(state, action), alpha, beta, depth + 1)
             if v2 < v:
                 v, move = v2, action
+                beta = min(beta, v)
+            if v <= alpha:
+                return (v, move)
         
         return (v, move)
